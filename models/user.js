f842932ae107a9ea58ad62f68beb625add6ea8b1
Nikita Klyushnikov <vipkonst@gmail.com>
2014-09-24 16:20:20 +0300

    Updated User Model

--- models/user.js
+++ models/user.js
@@ -2,6 +2,40 @@
     'use strict';
 
     var mongoose = require('../config/mongooseConfig');
+    var secret = require('../config/secret');
 
-    var mongoose
+    var SALT_WORK_FACTOR = secret.user.SALT_WORK_FACTOR;
+
+    var userSchema = mongoose.Schema({
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
+    });
+
+    userSchema.pre('save', function(next) {
+        var user = this;
+
+        if (!user.isModified('password')) return next();
+        
+        bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+            if (err) return next(err);
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+            });
+        });
+    });
+
+    userSchema.methods.comparePassword = function(password, cb) {
+        bcrypt.compare(password, this.password, function(err, isMatch) {
+            if (err) return cb(err);
+            cb(isMatch);
+        });
+    };
+
+    var userModel = mongoose.model('User', userSchema);
+
+    module.exports = userModel;
 })(module);
