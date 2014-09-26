6f94e03680f2fac1d6074cda13d473913edaec2d
Nikita Klyushnikov <vipkonst@gmail.com>
2014-09-26 17:31:07 +0300

    Updated mongoose config

--- config/mongooseConfig.js
+++ config/mongooseConfig.js
@@ -2,7 +2,10 @@
     var mongoose = require('mongoose');
     var mongoConfig = require('./mongoConfig');
 
-    mongoose.connect(mongoConfig.mongoConnectionString);
+    mongoose.connect(mongoConfig.mongoConnectionString, function(err) {
+        console.log('Mongo connection wasn\'t established, exiting application');
+        process.exit(1);
+    });
 
     module.exports = mongoose;
 })(module);
