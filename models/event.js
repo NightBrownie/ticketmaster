5452e87b8995825a844933523d6d57b808fdeb0a
NightBrownie <vipkonst@gmail.com>
2014-09-29 23:01:50 +0300

    Added event schema

---
+++ models/event.js
@@ -0,0 +1,17 @@
+(function(module, undefined) {
+    'use strict';
+
+    var mongoose = require('../config/mongooseConfig');
+    var ObjectId = mongoose.Schema.Types.ObjectId;
+
+    var eventSchema = mongoose.Schema({
+        date: {type: Date, required: true, default: Date.now()},
+        filmId: {type: ObjectId, required: true},
+        theaterId: {type: ObjectId, required: true},
+        hallId: {type: ObjectId, required: true}
+    });
+
+    //validatord
+
+    module.exports = mongoose.model('Event', userSchema);
+})(module);
